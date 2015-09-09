c88e3115bb2c3d5bd4a08a84872aee35978c2cbd
Sebastián <sebadlf@MacBook.local>
2015-09-09 16:05:28 -0300

    Gradle/Gulp Integration

---
+++ projects/reference-site/gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp'),
+    selenium = require('selenium-standalone'),
+    nightwatch = require('gulp-nightwatch');
+
+function errorLog(error){
+  console.error.bind(error);
+  this.emit('end');
+}
+
+gulp.task('selenium-connect', function (done) {
+
+  selenium.install({
+    logger: function (message) { }
+  }, function (err) {
+    if (err) return done(err);
+
+    selenium.start(function (err, child) {
+      if (err) return done(err);
+      selenium.child = child;
+      done();
+    });
+  });
+
+});
+
+gulp.task('tests', ['selenium-connect'], function() {
+
+  return gulp.src('')
+    .pipe(nightwatch())
+    .on('error', errorLog);
+});
+
+gulp.task('functional-tests', ['tests'], function () {
+
+  selenium.child.kill();
+
+});
+
+
